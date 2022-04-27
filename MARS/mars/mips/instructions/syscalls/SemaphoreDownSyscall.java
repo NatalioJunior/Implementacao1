53c1dbbe0f4cbec056ebdb8eb5e86bc220729b39
Arthur Felipe <arthurfellype21@gmail.com>
2022-04-26 22:06:40 -0300

    adding semaphoreDownSyscall

--- MARS/mars/mips/instructions/syscalls/SemaphoreDownSyscall.java
+++ MARS/mars/mips/instructions/syscalls/SemaphoreDownSyscall.java
@@ -1,18 +1,27 @@
 package mars.mips.instructions.syscalls;
+import mars.mips.SO.ProcessManager.*;
 import mars.util.*;
 import mars.*;
+import java.util.*;
 
 public class SemaphoreDownSyscall extends AbstractSyscall{
+	ArrayList<PCB> blockedList = new ArrayList<>();
+	SemaphoreCreateSyscall createSemaphore = new SemaphoreCreateSyscall();
 
 	public SemaphoreDownSyscall() {
 		super(65, "DownSemaphore");
 		// TODO Auto-generated constructor stub
+		createSemaphore.setInitialValue(createSemaphore.getInitialValue());
 	}
 
 	@Override
 	public void simulate(ProgramStatement statement) throws ProcessingException {
 		// TODO Auto-generated method stub
-		SystemIO.printString("Hello Syscall!\n");
+		if (blockedList.isEmpty() ){
+			createSemaphore.setInitialValue(createSemaphore.getInitialValue() - 1); //Decrementando valor inicial do semaforo
+		}else {
+			ProcessTable.incrementReady(blockedList.get(blockedList.size() - 1).getPID(), 2); // Alterando estado do ultimo item do semaforo para 'BLOQUEADO'
+		}
 	}
 
 }
